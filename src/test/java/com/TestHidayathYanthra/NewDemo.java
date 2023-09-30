557266c34a1c1d2bd83998ed79fd5e477ffc0231
Hidayath23 <shaikehidayath64@gmail.com>
2023-09-30 13:16:14 +0530

    First Commit

---
+++ src/test/java/com/TestHidayathYanthra/NewDemo.java
@@ -0,0 +1,13 @@
+package com.TestHidayathYanthra;
+
+import org.testng.annotations.Test;
+
+public class NewDemo 
+{
+	@Test
+	public void demoFirst()
+	{
+		System.out.println("First one");
+		System.out.println("Second one");
+	}
+}
